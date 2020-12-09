e1f5823874da3d9a50fd4451822e65f05e0cbe4a
Mike Wang <55117208+mikewang928@users.noreply.github.com>
2020-12-09 15:33:19 +0800

    internal logic changed (adding the numerator conv in testing)

--- DRRS_Z.m
+++ DRRS_Z.m
@@ -3,13 +3,18 @@
 
 
 % testing 
-testTF = DRRS_tf_Z(71,53,44100)
+testTF = DRRS_tf_Z(75,53,44100)
 % extract the coeificent from the tranfer function
 [num, dem] = tfdata(testTF)
+[hT, wT] = freqz(cell2mat(num), cell2mat(dem));
+plot((wT)/(2*pi), 20*log10(abs(hT)))
 % comvert the tranfer function into a dlit filter object
 [s,g] = zp2sos(cell2mat(num),cell2mat(dem),1);
 hf = dfilt.df2sos(s,g);
-fvtool(hf)
+% x is the raw sampled .wav file data 
+%filterTest = filter(num,dem,x)
+%fvtool(filterTest)
+%fvtool(hf) 
 
 %remember to add the normalization gain of (1/L1/L2) 
 % input: 
@@ -24,8 +29,9 @@ function DRRS = DRRS_tf_Z(L1,L2,F_s)
     numerator2 = [1, zeros(1, L2-1), -1];
     numeratorT = conv(numerator1, numerator2);
     denominator = [1, -1];
+    denominatorT = conv(denominator,denominator);
     ts = 1/F_s;
-    DRRS = 1/L1/L2*tf(numeratorT,denominator,ts,'Variable','z^-1');
+    DRRS = 1/L1/L2*tf(numeratorT,denominatorT,ts,'Variable','z^-1');
 end 
 
  
