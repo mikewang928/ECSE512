79c5cb0333cc0a3a0f7cb6cbc242cd4ed1ffa501
Mike Wang <55117208+mikewang928@users.noreply.github.com>
2020-12-09 17:46:07 +0800

    gain added

--- DRRSMBPSample.m
+++ DRRSMBPSample.m
@@ -1,31 +1,31 @@
 % testing 
 F_s = 44100;
-FL1 = 300;
-FH1 = 600;
-FL2 = 1500;
-FH2 = 1800;
-FL3 = 3000;
-FH3 = 3600;
-FL4 = 5000;
-FH4 = 5600;
-FL5 = 7500;
-FH5 = 8100;
+FL1 = 1000;
+FH1 = 3000;
+FL2 = 4000;
+FH2 = 6000;
+FL3 = 7000;
+FH3 = 9000;
+FL4 = 10000;
+FH4 = 12000;
+FL5 = 13000;
+FH5 = 15000;
 
-testBP = DRRS_5B_BandPass(FL1,FH1,FL2,FH2,FL3,FH3,FL4,FH4,FL5,FH5,F_s)
+testBP = DRRS_5B_BandPass(FL1,FH1,FL2,FH2,FL3,FH3,FL4,FH4,FL5,FH5,F_s,100,50,50,150,200)
 [num, dem] = tfdata(testBP)
 [hT, wT] = freqz(cell2mat(num), cell2mat(dem));
 
 plot((wT*F_s)/(2*pi), 20*log10(abs(hT)))
 
 
-function DRRS_5_tf = DRRS_5B_BandPass(FL1,FH1,FL2,FH2,FL3,FH3,FL4,FH4,FL5,FH5,F_s)
-    DRRS_5_tf = DRRS_2B_BandPass(FL1,FH1,FL2,FH2,F_s)+DRRS_2B_BandPass(FL3,FH3,FL4,FH4,F_s)+DRRS_1B_BandPass(FL5,FH5,F_s);
+function DRRS_5_tf = DRRS_5B_BandPass(FL1,FH1,FL2,FH2,FL3,FH3,FL4,FH4,FL5,FH5,F_s,factor1,factor2,factor3,factor4,factor5)
+    DRRS_5_tf = (DRRS_2B_BandPass(FL1,FH1,FL2,FH2,F_s,factor1,factor2)+DRRS_2B_BandPass(FL3,FH3,FL4,FH4,F_s,factor3,factor4)+DRRS_1B_BandPass(FL5,FH5,F_s,factor5));
 end
 
 
 
 % this will return a dual band pass filter tranfer fuction in z-domain 
-function DRRS_tf= DRRS_2B_BandPass(FL1,FH1,FL2,FH2,F_s)
+function DRRS_tf= DRRS_2B_BandPass(FL1,FH1,FL2,FH2,F_s,factor1,factor2)
     L11 = FindL1(FL1,F_s);
     L12 = findL2(L11);
     H11 = FindL1(FH1,F_s);
@@ -40,12 +40,12 @@ function DRRS_tf= DRRS_2B_BandPass(FL1,FH1,FL2,FH2,F_s)
     K_H_2 = 1/(H21*H22);
     ts = 1/F_s;
     z = tf('z',ts);
-    DRRS_tf = (K_H_1*(z^(-1*((L11+L12)/2)+1))*DRRS_tf_Z(H11,H12,F_s)-K_L_1*z^(-1*((H11+H12)/2)+1)*DRRS_tf_Z(L11,L12,F_s))+(K_H_2*(z^(-1*((L21+L22)/2)+1))*DRRS_tf_Z(H21,H22,F_s)-K_L_2*z^(-1*((H21+H22)/2)+1)*DRRS_tf_Z(L21,L22,F_s));
+    DRRS_tf = factor1*(K_H_1*(z^(-1*((L11+L12)/2)+1))*DRRS_tf_Z(H11,H12,F_s)-K_L_1*z^(-1*((H11+H12)/2)+1)*DRRS_tf_Z(L11,L12,F_s))+factor2*(K_H_2*(z^(-1*((L21+L22)/2)+1))*DRRS_tf_Z(H21,H22,F_s)-K_L_2*z^(-1*((H21+H22)/2)+1)*DRRS_tf_Z(L21,L22,F_s));
 end
 
 % this is a test file for a single bandpass filter designed by using drrs
 % technique 
-function BandPass = DRRS_1B_BandPass(FL1,FH1,F_s)
+function BandPass = DRRS_1B_BandPass(FL1,FH1,F_s,factor)
     L11 = FindL1(FL1,F_s);
     L12 = findL2(L11);
     H11 = FindL1(FH1,F_s);
@@ -54,7 +54,7 @@ function BandPass = DRRS_1B_BandPass(FL1,FH1,F_s)
     K_H_1 = 1/(H11*H12);
     ts = 1/F_s;
     z = tf('z',ts);
-    BandPass = K_H_1*(z^(-1*((L11+L12)/2)+1))*DRRS_tf_Z(H11,H12,F_s)-K_L_1*z^(-1*((H11+H12)/2)+1)*DRRS_tf_Z(L11,L12,F_s);
+    BandPass = factor*(K_H_1*(z^(-1*((L11+L12)/2)+1))*DRRS_tf_Z(H11,H12,F_s)-K_L_1*z^(-1*((H11+H12)/2)+1)*DRRS_tf_Z(L11,L12,F_s));
 end
 
 function DRRS = DRRS_tf_Z(L1,L2,F_s)
